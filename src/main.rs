acf9a1b87b5ee0cd2f7cafd7c3a6d552481f3a0f
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2022-05-10 18:42:27 +0800

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::io;
 use std::io::Write;
 
-fn main() {
+fn main() -> io::Result<()> {
+    // let stdout = io::stdout();
+    // let stdout = stdout.lock();
+    // let mut writer = io::BufWriter::new(stdout);
+    // let yes_bytes = b"y\n";
+    //
+    // loop {
+    //     writer.write(yes_bytes)?;
+    // }
+
+    let buffer = "y\n".repeat(4096).into_bytes();
     let stdout = io::stdout();
-    let mut writer = io::BufWriter::new(stdout.lock());
-    let yes_bytes = b"y\n";
+    let mut stdout = stdout.lock();
+
     loop {
-        writer.write(yes_bytes).unwrap();
+        stdout.write(&buffer).unwrap();
     }
 }
