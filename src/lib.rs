e935b58ee666c4125ebd8c9fa344330325da77bb
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2022-05-11 21:20:46 +0800

    yes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::env;
 pub fn get_buf() -> Vec<u8> {
-    const BUF_SIZE: usize = 4096;
+    // pages-aligned buffer
+    const BUF_SIZE: usize = 4096 * 2;
     let mut args: Vec<String> = env::args().collect();
     return if args.len() > 1 {
         args.remove(0);
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,6 @@ use std::io::Write;
 use yes::get_buf;
 
 fn main() -> io::Result<()> {
-    // let stdout = io::stdout();
-    // let stdout = stdout.lock();
-    // let mut writer = io::BufWriter::new(stdout);
-    // let yes_bytes = b"y\n";
-    //
-    // loop {
-    //     writer.write(yes_bytes)?;
-    // }
-
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
     let buf = get_buf();
